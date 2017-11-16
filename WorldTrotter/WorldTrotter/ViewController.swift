42a41e6e210ca5af6474aea8a357e827d0598a2e
Bart Jung <bart@Barts-MacBook-Pro.local>
2017-11-16 22:33:42 +0900

    WorldTrotter is added

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Bart Jung on 16/11/2017.
+//  Copyright © 2017 Bart Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+}
+
+
